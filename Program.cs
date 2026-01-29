63f1ed96cd964ec7e2ad9ef8c00795781ea75fb9
KevinEstupinan <kevin.estupinan.77@gmail.com>
2026-01-29 10:49:34 -0500

    reestructuracion Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,21 @@
-puclic class Hero
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
 app.MapGet("/", () =>
 {
-    // Create a Hero
-    var hero = new Hero { Id = 1, Name = "Windstorm" };
+    var hero = new Hero
+    {
+        Id = 1,
+        Name = "Windstorm"
+    };
 
-    // Return the Hero
     return Results.Ok(hero);
-
-    // return Results.Ok("Hello World");
-
 });
 
 app.Run();
+
+public class Hero
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
